b010881e870d7fea655640c0ae3113d16e320330
DeepaBaddur <deepa.baddur@cognizant.com>
2020-06-07 19:28:11 +0530

    Added Search class.

--- src/com/gittest/first/Test.java
+++ src/com/gittest/first/Test.java
@@ -3,7 +3,15 @@ package com.gittest.first;
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("Working with GIT is fun!");
+		// Let's write some code.
+		int arr[] = {2, 3, 4, 10, 18, 17, 40};
+		int x = 19;
+		Search search = new Search();
+		int result = search.search(arr, x);
+		if(result == -1)
+			System.out.println("Element is not present in the array");
+		else
+			System.out.println("Element is present at the index "+result);
 	}
 
 }
