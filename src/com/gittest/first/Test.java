c00648e05d48f6c39684ab0969023d72fb783e80
DeepaBaddur <deepa.baddur@cognizant.com>
2020-06-07 21:46:09 +0530

    Added changes in branch1.

--- src/com/gittest/first/Test.java
+++ src/com/gittest/first/Test.java
@@ -3,6 +3,7 @@ package com.gittest.first;
 public class Test {
 
 	public static void main(String[] args) {
+		System.out.println("Linearsearch algorithm");
 		// Let's write some code.
 		int arr[] = {2, 3, 4, 10, 18, 17, 40};
 		int x = 19;
